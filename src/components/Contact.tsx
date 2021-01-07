eae12d84c8a222101447f58fc38dd44c3073e97e
Le H Ngo <seiji@o2.pl>
2021-01-07 11:39:25 +0000

    email sending error message feedback if there was a problem sending it

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,15 +2,39 @@ import { FunctionComponent, useState, FormEvent } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { sendEmail } from "src/lib/api";
 import Link from "next/link";
+import CSS from "csstype";
+
+const emailErrStyle: CSS.Properties = {
+  background: "#99525d",
+  padding: "1rem",
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+};
+
+const emailErrMess: CSS.Properties = {
+  position: "absolute",
+  background: "#f4f4f4",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  padding: "2rem",
+  borderRadius: "10px",
+  color: "#9b9b9b",
+  fontSize: "0.8em",
+  cursor: "pointer",
+};
 
 const Contact: FunctionComponent = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [emailContent, setEmailContent] = useState<string>("");
 
+  const [emailError, setEmailError] = useState<boolean>(true);
+
   const emailSubmitHandler = async (e: FormEvent) => {
     e.preventDefault();
-    
+
     const emailBody = {
       name: name,
       email: email,
@@ -69,7 +93,20 @@ const Contact: FunctionComponent = () => {
           </div>
           <div className="image"></div>
         </div>
+
         <form onSubmit={emailSubmitHandler}>
+          {emailError ? (
+            <div style={emailErrStyle}>
+              <div
+                style={emailErrMess}
+                onClick={() => setEmailError(!emailError)}
+              >
+                There was a problem sending the form. Please try again later.
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
           <input
             placeholder="Enter Name"
             value={name}
